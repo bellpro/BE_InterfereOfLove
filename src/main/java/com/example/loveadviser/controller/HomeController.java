5ed37b586d870709517f989e379a36b3ac06aa8d
joorani <shinerani16@gmail.com>
2021-12-07 15:57:58 +0900

    게시글 전체 조회 추가

---
+++ src/main/java/com/example/loveadviser/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.loveadviser.controller;
+
+import com.example.loveadviser.dto.ArticleResponseDto;
+import com.example.loveadviser.service.ArticleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class HomeController {
+
+    private final ArticleService articleService;
+
+    //게시글 list 조회
+    @GetMapping("/home")
+    public List<ArticleResponseDto> getArticles() {
+        return articleService.getArticles();
+    }
+
+
+}
--- src/main/java/com/example/loveadviser/dto/ArticleResponseDto.java
+++ src/main/java/com/example/loveadviser/dto/ArticleResponseDto.java
@@ -1,9 +1,14 @@
 package com.example.loveadviser.dto;
 
+import com.example.loveadviser.model.Article;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -13,4 +18,20 @@ public class ArticleResponseDto {
     private String nickname;            // 닉네임
     private String title;               // 제목
     private LocalDateTime createDate;   // 생성일자
+
+    public static List<ArticleResponseDto> listOf(List<Article> articles) {
+        List<ArticleResponseDto> articleResponseDtos = new ArrayList<>();
+
+        for (Article article : articles) {
+            ArticleResponseDto articleResponseDto = new ArticleResponseDto(
+                    article.getArticle_id(),
+                    article.getType(),
+                    article.getUser().getNickname(),
+                    article.getTitle(),
+                    article.getCreatedAt()
+            );
+            articleResponseDtos.add(articleResponseDto);
+        }
+        return articleResponseDtos;
+    }
 }
