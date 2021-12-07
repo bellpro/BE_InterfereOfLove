01a743df64939d53af7bd524355bd7655aa91c39
joorani <shinerani16@gmail.com>
2021-12-07 23:39:32 +0900

    회원가입 데이터 유효성검사 - 에러처리로직 변경

--- src/main/java/com/example/loveadviser/validator/UserValidator.java
+++ src/main/java/com/example/loveadviser/validator/UserValidator.java
@@ -1,19 +1,7 @@
 package com.example.loveadviser.validator;
 
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-=======
-import com.example.loveadviser.dto.SignupRequestDto;
-import com.example.loveadviser.model.User;
-import com.example.loveadviser.repository.UserRepository;
-import com.example.loveadviser.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.util.ObjectUtils;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
->>>>>>> b04626cc0cfdee84db0b73c0b38938f2857a7871
 
 import java.util.regex.Pattern;
 
@@ -32,17 +20,18 @@ public class UserValidator {
 
         //아이디 검사
         if(username == null || !Pattern.matches(patternId, username)){
-            throw new IllegalArgumentException("숫자,영문 포함하여 3~12자 이내로 입력해주세요.");
+            throw new IllegalArgumentException("아이디는 숫자,영문 포함하여 3~12자 이내로 입력해주세요.");
         }
 
         //닉네임 검사
         if (nickname == null || nickname.length() > 10) {
-            throw new IllegalArgumentException("닉네임은 10자 이내로 입력하세요.");
+            throw new IllegalArgumentException("닉네임은 10자 이내로 입력해주세요.");
         }
 
         //비밀번호 확인
         if (password == null || !Pattern.matches(patternpw, password)) {
-            throw new IllegalArgumentException("숫자,문자,특수문자 포함하여 4~12자 이내로 입력해주세요.");
+            throw new IllegalArgumentException(
+                    "비밀번호는 숫자,문자,특수문자 한 문자 이상 포함하여 4~12자 이내로 입력해주세요.");
         }
 
         if (!password.equals(password2)) {
