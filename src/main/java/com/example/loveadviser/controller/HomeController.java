f55b624ceb428fcc1a9c49a9f164daf3f14a065a
joorani <shinerani16@gmail.com>
2021-12-07 20:47:31 +0900

    게시글 조회 swagger 추가

--- src/main/java/com/example/loveadviser/controller/HomeController.java
+++ src/main/java/com/example/loveadviser/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.loveadviser.controller;
 
 import com.example.loveadviser.dto.ArticleResponseDto;
 import com.example.loveadviser.service.ArticleService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class HomeController {
     private final ArticleService articleService;
 
     //게시글 list 조회
+    @ApiOperation("모든 게시글 조회")
     @GetMapping("/home")
     public List<ArticleResponseDto> getArticles() {
         return articleService.getArticles();
